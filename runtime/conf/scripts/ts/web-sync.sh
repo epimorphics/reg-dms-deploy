d208f4bfdb69536b34951566760819cb58d23950
der <dave@epimorphics.com>
2015-07-13 22:15:52 +0100

    Minor clean ups as part of documenting sample config

--- runtime/conf/scripts/ts/web-sync.sh
+++ runtime/conf/scripts/ts/web-sync.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-# Post-publication actions for the ts service
+# Web sync operation
 # Arguments:
-#     serversDir 
+#     serversDir (for presentation servers)
 
 
 set -o errexit
 [[ $# = 1 ]] || { echo "Internal error calling $0" 1>&2 ; exit 1 ; }
 . ./config.sh
 
-readonly serversDir="/var/opt/dms/$1"
+readonly serversDir="$1"
 
 echo "Synchronizing web content, including source/dump files"
 cd $serversDir/../../Web
@@ -16,13 +16,9 @@ for server in $serversDir/servers/*
 do
     if grep -qv Terminated $server/status 
     then
-        FLAGS="$SSH_FLAGS -i /var/opt/dms/.ssh/${PREFIX}.pem"
+        FLAGS="$SSH_FLAGS -i /var/opt/dms/.ssh/lds.pem"
         echo "Sync to $server"
         IP=$( jq -r .address "$server/config.json" )
-        rsync -a --delete -e "ssh $FLAGS" * ubuntu@$IP:/var/www/html
-
-        echo "Clear caches"
-        ssh -t -t $FLAGS -l ubuntu $IP sudo /usr/local/bin/ps_cache_clean 
+        rsync -a --delete -e "ssh $FLAGS" * ubuntu@$IP:/var/www/environment/html
     fi
 done
-
